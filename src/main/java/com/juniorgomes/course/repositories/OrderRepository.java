c6f80b38f2a0937295dece1b21c78a5b9c83d64c
Junior Gomes <josivaldo.ifpb@gmail.com>
2022-09-20 20:33:13 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/juniorgomes/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.juniorgomes.course.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.juniorgomes.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	
+}
