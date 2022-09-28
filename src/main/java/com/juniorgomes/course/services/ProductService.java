177422cffd86695e5fddd8718ac0d91a48e75801
Junior Gomes <josivaldo.ifpb@gmail.com>
2022-09-28 12:32:26 -0300

    Product

---
+++ src/main/java/com/juniorgomes/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.juniorgomes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juniorgomes.course.entities.Product;
+import com.juniorgomes.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
